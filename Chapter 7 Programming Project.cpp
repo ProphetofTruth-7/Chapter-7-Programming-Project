5380cfe50355482180f67e058ef4fc058061f570
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-15 17:32:11 -0500

    LNK1168 Error

--- Chapter 7 Programming Project.cpp
+++ Chapter 7 Programming Project.cpp
@@ -3,10 +3,39 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <cstdlib>
 using namespace std;
+
+void teamGathering(string x, vector<string>& TeamVector);
 
 int main()
 {
-    cout << "Main Branch!";
+    string teamNames = "TeamNames.txt";
+    string worldSeriesVictories = "WorldSeriesChamps.txt";
+
+    vector<string> TeamNameVector;
+
+    teamGathering(teamNames,TeamNameVector);
+
+
     return 0;
+}
+
+
+// Functions Below
+
+void teamGathering(string x, vector<string>& TeamVector) {
+    ifstream inFile(x);
+    string line;
+
+    if (!inFile) {
+        cerr << "Error opening file!" << endl;
+    }
+    getline(inFile, line);
+
+    TeamVector.push_back(line);
+    cout << TeamVector.at(1) << endl;
+    
+    inFile.close();
 }
